6f53278c9f02df7686c806786f426629be7047a8
nicolas-magalhaes-clear <nicolas.magalhaes.clear@gmail.com>
2023-11-07 12:19:29 -0300

    Button interaction with animation

--- App.jsx
+++ App.jsx
@@ -1,66 +1,67 @@
 import { StatusBar } from 'expo-status-bar';
 import { Component } from 'react';
 
-import { StyleSheet, Text, View, Animated } from 'react-native';
+import { StyleSheet, Text, View, Animated, TouchableOpacity } from 'react-native';
 
-export default class App extends Component{
+export default class App extends Component {
 
   constructor(props) {
     super(props)
     this.state = {
       LarAnimada: new Animated.Value(150),
       AltAnimada: new Animated.Value(50),
-      OpacidadeAnimada: new Animated.Value(1)
+      OpacidadeAnimada: new Animated.Value(0)
     };
 
+    this.carregarGrafico = this.carregarGrafico.bind(this)
 
-    Animated.loop(
-      Animated.sequence([
-        Animated.parallel([
-          Animated.timing(
-            this.state.LarAnimada,
-            {
-              toValue: 300,
-              duration: 700
-            }
-          ),
-          Animated.timing(
-            this.state.AltAnimada,
-            {
-              toValue: 100,
-              duration: 700
-            }
-          )
-        ]),
-        Animated.parallel([
-          Animated.timing(
-            this.state.LarAnimada,
-            {
-              toValue: 150,
-              duration: 700
-            }
-          ),
-          Animated.timing(
-            this.state.AltAnimada,
-            {
-              toValue: 50,
-              duration: 700
-            }
-          )
-        ])
-      ])
-    ).start()
 
-    
+
+
+  }
+
+  carregarGrafico() {
+    Animated.sequence([
+      Animated.timing(
+        this.state.OpacidadeAnimada,
+        {
+          toValue: 1,
+          duration: 500
+        }
+      ),
+      Animated.timing(
+        this.state.AltAnimada,
+        {
+          toValue: 300,
+          duration: 1000
+        }
+      )
+    ]).start()
   }
 
 
   render() {
     return (
-      <View style={styles.container}>
-        <Animated.View style={{ width: this.state.LarAnimada, height: this.state.AltAnimada, opacity: this.state.OpacidadeAnimada, backgroundColor: 'blue', justifyContent: 'center', alignItems: 'center' }}>
-          <Text style={{ color: '#FFF' }}>Carregando...</Text>
-        </Animated.View>
+      <View style={{ flex: 1, marginTop: 35 }}>
+
+        <View style={{ height: 80, alignItems: 'center', flexDirection: 'row', justifyContent: 'center', backgroundColor: '#4169E1' }}>
+          <TouchableOpacity onPress={this.carregarGrafico()}>
+            <Text>Gerar gráfico</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={{ flex: 1, alignItems: 'center', flexDirection: 'column', justifyContent: 'flex-end' }}>
+          <Text>Vendas</Text>
+          <Animated.View style={{ width: this.state.LarAnimada, height: this.state.AltAnimada, opacity: this.state.OpacidadeAnimada, backgroundColor: '#FF0000', justifyContent: 'center', alignItems: 'center' }}>
+            <Text style={{ color: '#FFF' }}>R$ 150,00</Text>
+          </Animated.View>
+
+        </View>
+
+
+
+
+
       </View>
     );
   }
